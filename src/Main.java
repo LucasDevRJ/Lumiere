73f941631c2f542a8da77563034a31c7563ede2f
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-25 19:09:01 -0300

    Declarando variáveis e as exibindo no terminal.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2000;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 7.32;
+
+        System.out.println("Bem-vindo ao Lumière");
+        System.out.println("Filme: Gladiador");
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Disponível: " + incluidoNoPlano);
+        System.out.println("Nota: " + notaDoFilme);
+    }
+}
