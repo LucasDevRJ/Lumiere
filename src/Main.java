27d338708154996ffd657c9ebbc91e109d3200a9
Lucas <lucaspereiradelima2020@gmail.com>
2025-10-26 14:40:53 -0300

    Separando o código em variáveis, declaração de valores e exibição de conteúdo formatado.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,37 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int anoDeLancamento = 2000;
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 7.32;
+
+        String titulo;
+        String sinopse;
+        int anoDeLancamento;
+        int quantidadeDeNotas;
+        boolean incluidoNoPlano;
+        double notaDoFilme1;
+        double notaDoFilme2;
+        double notaDoFilme3;
+        double mediaDasNotas;
+
+        titulo = "Gladiador";
+        sinopse = """
+                A sinopse de %s é sobre Maximus, um leal general romano que é traído pelo ambicioso filho 
+                do imperador, Commodus, o qual mata seu pai e assume o trono. Ordem de Commodus, Maximus é dado como 
+                morto e é forçado a se tornar um escravo, sendo treinado como gladiador, em busca de vingança contra 
+                o imperador.
+                """.formatted(titulo);
+        anoDeLancamento = 2000;
+        quantidadeDeNotas = 3;
+        incluidoNoPlano = true;
+        notaDoFilme1 = 7.2;
+        notaDoFilme2 = 6.3;
+        notaDoFilme3 = 8.0;
+        mediaDasNotas = (notaDoFilme1 + notaDoFilme2 + notaDoFilme3) / quantidadeDeNotas;
 
         System.out.println("Bem-vindo ao Lumière");
         System.out.println("Filme: Gladiador");
+        System.out.println(sinopse);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         System.out.println("Disponível: " + incluidoNoPlano);
-        System.out.println("Nota: " + notaDoFilme);
+        System.out.printf("Nota: %.1f", mediaDasNotas);
     }
 }
