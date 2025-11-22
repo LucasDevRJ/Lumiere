d715c6e785a7c070f02f414024fd9f555332e8bd
Lucas <lucaspereiradelima2020@gmail.com>
2025-11-22 10:58:45 -0300

    Utilizando o método acessor.

--- src/Filme.java
+++ src/Filme.java
@@ -1,15 +1,25 @@
 public class Filme {
+
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    private double avaliacao;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
     int duracaoEmMinutos;
 
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
     void exibeFichaTecnica() {
         System.out.println("Nome do Filme: " + nome);
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avaliaFilme(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
     }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,18 @@
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "Sonic";
-        meuFilme.anoDeLancamento = 2020;
-        meuFilme.incluidoNoPlano = true;
-        meuFilme.avaliacao = 8.1;
-        meuFilme.totalDeAvaliacoes = 12456;
-        meuFilme.duracaoEmMinutos = 99;
 
-        meuFilme.exibeFichaTecnica();
+        Filme filme1 = new Filme();
+        filme1.nome = "Sonic - O Filme";
+        filme1.anoDeLancamento = 2020;
+        filme1.incluidoNoPlano = true;
+        filme1.duracaoEmMinutos = 99;
+
+        filme1.avaliaFilme(7.0);
+        filme1.avaliaFilme(6.0);
+        filme1.avaliaFilme(9.0);
+
+        filme1.exibeFichaTecnica();
+
+        System.out.println(String.format("Total de avaliações do filme \"%s\": %d", filme1.nome, filme1.getTotalDeAvaliacoes()));
     }
 }
