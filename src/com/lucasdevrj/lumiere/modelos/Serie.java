6e2bb5195e88f0b2c653649a41ec9dd4a9c2f123
Lucas <lucas.pereira@animati.com.br>
2025-12-04 14:30:42 -0300

    Adição dos atributos.

--- src/com/lucasdevrj/lumiere/modelos/Serie.java
+++ src/com/lucasdevrj/lumiere/modelos/Serie.java
@@ -1,4 +1,14 @@
 package com.lucasdevrj.lumiere.modelos;
 
 public class Serie {
+
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private int somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+    private int temporadas;
+    private int episodiosPorTemporada;
 }
+
