54ae18f02c6e614a3889f7576e2c15e1dbb48e3e
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-11 19:22:12 -0300

    Criando Record

---
+++ src/com/lucasdevrj/lumiere/modelos/TituloOmdb.java
@@ -0,0 +1,4 @@
+package com.lucasdevrj.lumiere.modelos;
+
+public record TituloOmdb(String title, String runtime, String genre, int year, String director, String actors, String awards, String poster, String country) {
+}
