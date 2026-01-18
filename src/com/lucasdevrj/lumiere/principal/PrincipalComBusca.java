2fcc99cff6b7be6094fcb57f41a34160075fdc3c
Lucas <lucaspereiradelima2020@gmail.com>
2026-01-18 15:05:53 -0300

    Outro tratamento de Erro

--- src/com/lucasdevrj/lumiere/principal/PrincipalComBusca.java
+++ src/com/lucasdevrj/lumiere/principal/PrincipalComBusca.java
@@ -22,36 +22,44 @@ public class PrincipalComBusca {
         busca = busca.replaceAll(" ", "+");
         String url = "https://www.omdbapi.com/?apikey=fa6df38e&t=" + busca;
 
-        HttpClient cliente = HttpClient.newHttpClient();
-        HttpRequest requisicao = HttpRequest.newBuilder()
-                        .uri(URI.create(url))
-                        .build();
-        HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
-        String json = resposta.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println("--------------------|Record do Título|--------------------");
-        System.out.println("Título: " + tituloOmdb.title());
-        System.out.println("Duração: " + tituloOmdb.runtime());
-        System.out.println("Ano: " + tituloOmdb.year());
-        System.out.println("Gênero: " + tituloOmdb.genre());
-        System.out.println("Diretor: " + tituloOmdb.director());
-        System.out.println("Atores: " + tituloOmdb.actors());
-        System.out.println("Prêmios: " + tituloOmdb.awards());
-        System.out.println("Poster: " + tituloOmdb.poster());
-        System.out.println("País: " + tituloOmdb.country());
-        System.out.println("----------------------------------------------------------");
-
         try {
-            Titulo titulo = new Titulo(tituloOmdb);
-            System.out.println("--------------------|Classe Título|--------------------");
-            System.out.println(titulo);
-            System.out.println("-------------------------------------------------------");
-        } catch (NumberFormatException e) {
-            System.out.print("Aconteceu um erro: ");
-            System.out.print(e.getMessage());
+            HttpClient cliente = HttpClient.newHttpClient();
+            HttpRequest requisicao = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+            HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
+            String json = resposta.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println("--------------------|Record do Título|--------------------");
+            System.out.println("Título: " + tituloOmdb.title());
+            System.out.println("Duração: " + tituloOmdb.runtime());
+            System.out.println("Ano: " + tituloOmdb.year());
+            System.out.println("Gênero: " + tituloOmdb.genre());
+            System.out.println("Diretor: " + tituloOmdb.director());
+            System.out.println("Atores: " + tituloOmdb.actors());
+            System.out.println("Prêmios: " + tituloOmdb.awards());
+            System.out.println("Poster: " + tituloOmdb.poster());
+            System.out.println("País: " + tituloOmdb.country());
+            System.out.println("----------------------------------------------------------");
+
+            try {
+                Titulo titulo = new Titulo(tituloOmdb);
+                System.out.println("--------------------|Classe Título|--------------------");
+                System.out.println(titulo);
+                System.out.println("-------------------------------------------------------");
+            } catch (NumberFormatException erro) {
+                System.out.println("Erro de formatação de valor!");
+                System.out.println("Aconteceu um erro: " + erro.getMessage());
+            }
+
+        } catch (IllegalArgumentException erro) {
+            System.out.println("Valor inválido digitado!");
+            System.out.println("Erro: " + erro.getMessage());
+        } finally {
+            System.out.println("\nPrograma finalizado.");
         }
     }
 }
